15a65bb0b0f82c51e41d39e10fdfa51404524403
petudeveloper <davidalvarezmazzo@gmail.com>
2021-11-21 23:29:46 -0500

    Update controllers, views and models to handle name field+Bootstrap

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,6 @@
 class HomeController < ApplicationController
-  def index; end
+  before_action :authenticate_user!
+  def index
+    @user = current_user
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_22_014536) do
+ActiveRecord::Schema.define(version: 2021_11_22_035946) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_11_22_014536) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "admin", default: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
